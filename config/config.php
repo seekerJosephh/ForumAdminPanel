903f6fbb65cbc99ec8804d4b76a45b7b8dbb552d
Pang <chetra.pang.2064319@gmail.com>
2025-04-28 13:53:18 +0700

    updated

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ $db_name = 'forum';
 $username = 'e2g7wad_php';
 $password = 'm,LHqb?vugO%'; // Leave blank if no password
 try {
-  $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
